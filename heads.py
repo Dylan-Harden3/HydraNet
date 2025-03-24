b80498a7246a24c20ecc042bb1ff562f5b3d0837
Dylan Harden <dylanharden33@gmail.com>
2025-03-24 13:44:22 -0500

    forward pass for object detection head

--- heads.py
+++ heads.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 from common import ConvBlock, FastNormalizedFusion, ConvTransposeBlock
 import torch.nn.functional as F
-
+import torch
 
 class SegmentationHead(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -46,3 +46,47 @@ class SegmentationHead(nn.Module):
         )
         out = self.out(up2)
         return out
+
+class ObjectDetectionHead(nn.Module):
+    def __init__(self, in_channels, num_classes, num_anchors=3):
+        # for each anchor:
+        # x, y, h, w, object score + per class probabilites
+        out_channels = num_anchors * (5 + num_classes)
+        self.num_classes = num_classes
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 1)
+        self.conv2 = nn.Conv2d(in_channels, out_channels, 1)
+        self.conv3 = nn.Conv2d(in_channels, out_channels, 1)
+        self.conv4 = nn.Conv2d(in_channels, out_channels, 1)
+
+
+    def forward(self, p1, p2, p3, p4):
+        p1 = self.conv1(p1)
+        p2 = self.conv2(p2)
+        p3 = self.conv3(p3)
+        p4 = self.conv4(p4)
+        
+        return p1, p2, p3, p4
+
+
+def decode_object_predictions(preds, anchors, strides, num_classes):
+    # preds: 4, B, 3 x (5 + num_classes), H, W
+    # anchors: 4, 3, 2
+    # strides: 4, 2
+
+    outputs = []
+    for i, pred in enumerate(preds):
+        B, C, H, W = pred.shape
+        pred = pred.view(B, 3, 5+num_classes, H, W).permute(0, 3, 4, 1, 2)
+
+        grid_y, grid_x = torch.meshgrid(torch.arange(H), torch.arange(W), indexing='ij')
+        grid = torch.stack((grid_x, grid_y), dim=-1).float().to(pred.device)
+
+        pred[..., 0:2] = torch.sigmoid(pred[..., 0:2] + grid) * strides[i]
+        pred[..., 2:4] = torch.exp(pred[..., 2:4]) * anchors[i]
+        pred[..., 4:] = torch.sigmoid(pred[..., 4:])
+
+        pred = pred.reshape(B, H * W * 3, 5 + num_classes)
+        outputs.append(pred)
+    
+    return torch.cat(outputs, dim=1)
